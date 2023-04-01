8b52863e0d1a8973061678b10f712fe8a70553d8
Nicola Andrinirina <nicolas.aps.p2@gmail.com>
2023-04-01 23:49:44 +0300

    TP2 Exercice 4

---
+++ src/main/java/jsf/BeanExo4.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+package jsf;
+
+import jakarta.inject.Named;
+import jakarta.enterprise.context.RequestScoped;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author nicol
+ */
+@Named(value = "beanexo4")
+@RequestScoped
+public class BeanExo4 {
+
+    private int nombre;
+
+    public int getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(int nombre) {
+        this.nombre = nombre;
+    }
+
+    public List<Integer> getNombresSuivants() {
+        int nb = 5;
+        List<Integer> l = new ArrayList<>(nb);
+        for (int i = nombre; i < nombre + nb; i++) {
+            l.add(i);
+        }
+        return l;
+    }
+    
+    public String afficher() {
+        return "affichage_4?nb="+ nombre + "&amp;faces-redirect=true";
+    }
+    
+}
