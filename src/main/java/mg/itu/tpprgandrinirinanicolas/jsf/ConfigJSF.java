d43049530ccc6c5b00d16b8556901475fd691f13
Nicola Andrinirina <nicolas.aps.p2@gmail.com>
2023-04-01 23:48:46 +0300

    TP2 Exercice 1

---
+++ src/main/java/mg/itu/tpprgandrinirinanicolas/jsf/ConfigJSF.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mg.itu.tpprgandrinirinanicolas.jsf;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.faces.annotation.FacesConfig;
+
+/**
+ *
+ * @author nicol
+ */
+@ApplicationScoped
+@FacesConfig
+public class ConfigJSF {
+    
+}
