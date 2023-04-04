70c2680c9fa8c69d0133212568ac1ca0543e193f
Nicola Andrinirina <nicolas.aps.p2@gmail.com>
2023-04-05 00:15:05 +0300

    Paramétrage du template

--- src/main/java/jsf/BeanExo4.java
+++ src/main/java/jsf/BeanExo4.java
@@ -37,7 +37,7 @@ public class BeanExo4 {
     }
     
     public String afficher() {
-        return "affichage_4?nb="+ nombre + "&amp;faces-redirect=true";
+        return "affichage__4?nb="+ nombre + "&amp;faces-redirect=true";
     }
     
 }
